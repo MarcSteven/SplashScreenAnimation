d81f2e2d6e6123e085d0487bb3c8f6626d29552c
Marc Steven <MarcSteven@users.noreply.github.com>
2020-06-13 22:19:28 +0800

    complete the animation

--- Fuber/SplashScreen.swift
+++ Fuber/SplashScreen.swift
@@ -33,12 +33,34 @@ struct SplashScreen: View {
   let fuberBlue = Color("Fuber blue")
   let uLineWidth: CGFloat = 5
   let uZoomFactor: CGFloat = 1.4
+  let lineWidth:CGFloat = 4
+  let lineHeight:CGFloat = 28
+  
   
   @State var percent = 0.0
   @State var uScale: CGFloat = 1
+  @State var squareColor = Color.white
+  @State var uScale:CGFloat = 1
+  @State var squareScale:CGFloat = 1
+  @State var lineScale:CGFloat = 1
+  @State var textAlpha = 0.0
+  @State var coverCircleScale:CGFloat = 1
+  @State var coverCircleAhpha = 0.0
+  
   
   var body: some View {
     ZStack {
+            Image("Chimes")
+        .resizable(resizingMode: .tile)
+        .opacity(textAlpha)
+        .scaleEffect(textScale)
+      
+      Circle()
+        .fill(fuberBlue)
+        .frame(width: 1, height: 1, alignment: .center)
+        .scaleEffect(coverCircleScale)
+        .opacity(coverCircleAlpha)
+
       Text("F           BER")
         .font(.largeTitle)
         .foregroundColor(.white)
@@ -55,6 +77,19 @@ struct SplashScreen: View {
         }
         .scaleEffect(uScale * uZoomFactor)
         .frame(width: 45, height: 45, alignment: .center)
+      Rectangle()
+        .fill(squareColor)
+        .scaleEffect(squareScale * uZoomFactor)
+        .frame(width: uSquareLength, height: uSquareLength, alignment: .center)
+        .onAppear() {
+          self.squareColor = self.fuberBlue
+        }
+      
+      Rectangle()
+        .fill(fuberBlue)
+        .scaleEffect(lineScale, anchor: .bottom)
+        .frame(width: lineWidth, height: lineHeight, alignment: .center)
+        .offset(x: 0, y: -22)
       
       Spacer()
     }
@@ -67,6 +102,11 @@ struct SplashScreen: View {
 
 extension SplashScreen {
   var uAnimationDuration: Double { return 1.0 }
+  var uAnimationDelay:Double { return 0.2}
+  var uExitAnimationDuration:Double {return 0.4}
+  var minAnimationInterval:Double {return 0.1}
+  var finalAnimationDuration:Double {return 0.4}
+  var fadeAnimationDuration:Double {return 0.4}
   
   func handleAnimations() {
     runAnimationPart1()
@@ -77,9 +117,50 @@ extension SplashScreen {
     withAnimation(.easeIn(duration: uAnimationDuration)) {
       percent = 1
       uScale = 5
+      lineScale = 1
+    }
+    withAnimation(Animation.easeIn(duration:uAnimationDuration)
+                  .delay(0.5)) {
+      textAlpha = 1.0
+  }
+    let deadline: DispatchTime = .now() + uAnimationDuration + uAnimationDelay
+    DispatchQueue.main.asyncAfter(deadline: deadline) {
+      withAnimation(.easeOut(duration: self.uExitAnimationDuration)) {
+        self.uScale = 0
+        self.lineScale = 0
+      }
+      
+      withAnimation(.easeOut(duration: self.minAnimationInterval)) {
+        self.squareScale = 0
+      }
+      
+      withAnimation(Animation.spring()) {
+        self.textScale = self.uZoomFactor
+      }
+    }
+  }
+  func runAnimationPart2() {
+    let deadline: DispatchTime = .now() + uAnimationDuration +
+      uAnimationDelay + minAnimationInterval
+    DispatchQueue.main.asyncAfter(deadline: deadline) {
+      self.squareColor = Color.white
+      self.squareScale = 1
+      
+      withAnimation(.easeOut(duration: self.fadeAnimationDuration)) {
+        self.coverCircleAlpha = 1
+        self.coverCircleScale = 1000
+      }
     }
   }
   
+  func runAnimationPart3() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2 * uAnimationDuration) {
+      withAnimation(.easeIn(duration: self.finalAnimationDuration)) {
+        self.textAlpha = 0
+        self.squareColor = self.fuberBlue
+      }
+    }
+  }
   func restartAnimation() {
     let deadline: DispatchTime = .now() + uAnimationDuration
     DispatchQueue.main.asyncAfter(deadline: deadline) {
